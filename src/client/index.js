1644e29aaac6a91bc12f1194e0160f6d27aebd61
Sezerims <bullalioglu@gmail.com>
2023-09-22 18:46:02 +0300

    Add anonymous voting (experimental).

--- src/client/index.js
+++ src/client/index.js
@@ -5,6 +5,14 @@ let vote;
 let votes = document.querySelectorAll(".vote")
 let vs = document.querySelectorAll(".vote span");
 
+let voters = document.querySelectorAll(".voter-name");
+
+let listOfVoters = [];
+
+voters.forEach(voter => {
+    listOfVoters.push(voter.innerText);
+});
+
 let voteCounter = 0;
 
 document.querySelector("#vote-counter").innerText = voteCounter;
@@ -26,11 +34,34 @@ cards.forEach(card => card.addEventListener("click", () => {
     document.querySelector("#vote-counter").innerText = voteCounter;
 }));
 
-let toggle = document.querySelector("#toggle");
+let secretVoting = document.querySelector("#secret-voting");
+let anonVoting = document.querySelector("#anon-voting");
 
-toggle.addEventListener("click", () => {
-    toggle.classList.toggle("bi-eye-fill");
-    toggle.classList.toggle("bi-eye-slash-fill");
+secretVoting.addEventListener("click", () => {
+    secretVoting.classList.toggle("bi-eye-fill");
+    secretVoting.classList.toggle("bi-eye-slash-fill");
     votes.forEach(vote => vote.classList.toggle("face-down"));
     vs.forEach(v => v.classList.toggle("hidden"));
+});
+
+anonVoting.addEventListener("click", () => {
+    if(anonVoting.classList.contains("bi-person-fill")) {
+        let ctr = 1;
+        voters.forEach(voter => {
+            voter.innerText = "Anonymous Voter #" + ctr;
+            ctr++;
+        });
+    }
+    else {
+        let ctr = 0;
+        voters.forEach(voter => {
+            voter.innerText = listOfVoters[ctr];
+            ctr++;
+        });
+    }
+    anonVoting.classList.toggle("bi-person-fill");
+    anonVoting.classList.toggle("bi-incognito");
+    // voters.forEach(voter => voter.classList.toggle("face-down"));
+
+    voters.forEach(voter => voter.classList.toggle("selected"));
 });
